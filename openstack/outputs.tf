30eef942d3cca9b9f426e65ba3d491418938cfed
RsssTOP <f.frosti228@gmail.com>
2025-12-03 15:06:15 +0300

    Update outputs.tf

--- openstack/outputs.tf
+++ openstack/outputs.tf
@@ -1,3 +1,4 @@
-output "elk_public_ip" {
-  value = openstack_networking_floatingip_v2.elk_fip.address
+output "elk_vm_ip" {
+  description = "IP-адрес ВМ в сети sutdents-net"
+  value       = openstack_compute_instance_v2.elk_vm.network[0].fixed_ip_v4
 }
